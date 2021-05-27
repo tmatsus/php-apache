a9346dd5984085a12a9044dfec4832ca2488f98e
tmatsus <60866142+tmatsus@users.noreply.github.com>
2021-05-27 17:43:34 +0900

    Update view.php

--- htdocs/php/view.php
+++ htdocs/php/view.php
@@ -5,8 +5,8 @@
   $dbName = 'chapter6_db';
   $dbServer = getenv('DB_SERVER');
   $host = 'mysql3';
-  #$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8mb4";
-  $dsn = "mysql:host={$host};port=8889;dbname={$dbName};charset=utf8mb4";
+  $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+  #$dsn = "mysql:host={$host};port=8889;dbname={$dbName};charset=utf8mb4";
   #$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
 ?>
 
